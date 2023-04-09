ea7e1febec21ef97d44b10c28a0f9825463a4b46
Timofei Kurohtin <forgwent@gmail.com>
2023-04-09 22:29:36 +0300

    Fix overflow

--- src/components/RightSidebar/index.jsx
+++ src/components/RightSidebar/index.jsx
@@ -1,7 +1,7 @@
 import DiagramProperties from "./components/DiagramProperties";
 
 const LeftSidebar = () => {
-    return <aside className="w-60 p-2 border-l border-black">
+    return <aside className="w-60 p-2 border-l border-black overflow-auto">
         <DiagramProperties/>
     </aside>;
 };
